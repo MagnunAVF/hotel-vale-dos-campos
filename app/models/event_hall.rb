5dcb33ea1074cd35da3f58a70b1fd5001dd59adc
trap404 <magnunavf@gmail.com>
2017-05-26 01:39:59 -0300

    feature: Create Event Hall model

---
+++ app/models/event_hall.rb
@@ -0,0 +1,2 @@
+class EventHall < Accommodation
+end
---
+++ test/models/event_hall_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class EventHallTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
