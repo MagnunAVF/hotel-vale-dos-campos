8e3b85042bbc0dc081320537445636f3001d59c9
trap404 <magnunavf@gmail.com>
2017-05-28 12:29:28 -0300

    feature: Create initial DB file (seed)
    - Add price in accommodation validation

--- app/models/accommodation.rb
+++ app/models/accommodation.rb
@@ -1,6 +1,6 @@
 class Accommodation < ApplicationRecord
-  validates_presence_of :number, :capacity, :description, :occupied
-  validates_numericality_of :number, :capacity
+  validates_presence_of :number, :capacity, :description, :occupied, :price
+  validates_numericality_of :number, :capacity, :price
   validates_uniqueness_of :number
 
   has_many :bookings
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,85 @@
-if Hotelinformation.all.empty?
-  Hotelinformation.create(description: "Hotel localizado na Serra do RS.", email:"hotelvaledoscampos@gmail.com", fone: "51999998877", address: "Rua X num 123")
-else
-  print "HotelInformation exists !"
+print "*** Creating initial DB ***"
+print "Seeding DB ..."
+
+print "Creating Hotel Information"
+Hotelinformation.create(
+  description: "Hotel localizado na Serra do RS.",
+  email:"hotelvaledoscampos@gmail.com",
+  fone: "51999998877",
+  address: "Rua X num 123"
+)
+
+print "Creating Accommodations ..."
+number_fixture = 1
+boolean_fixture = false
+
+print "Creating Rooms "
+
+for i in 1..50
+  Room.create(
+    number: number_fixture,
+    capacity: 2,
+    description: "Quarto regular para casal",
+    price: 170.00,
+    occupied: boolean_fixture,
+    single_beds_number: 0,
+    couple_beds_number: 1
+  )
+  number_fixture += 1
+  boolean_fixture = !boolean_fixture
+end
+
+for i in 1..30
+  Room.create(
+    number: number_fixture,
+    capacity: 2,
+    description: "Quarto regular",
+    price: 180.00,
+    occupied: boolean_fixture,
+    single_beds_number: 2,
+    couple_beds_number: 0
+  )
+  number_fixture += 1
+  boolean_fixture = !boolean_fixture
+end
+
+print "Creating Meeting Rooms"
+for i in 1..10
+  MeetingRoom.create(
+    number: number_fixture,
+    capacity: 25,
+    description: "Sala de reuniões com computador, projetor e uma mesa redonda",
+    price: 200.00,
+    occupied: boolean_fixture,
+    videoconf: false
+  )
+  number_fixture += 1
+  boolean_fixture = !boolean_fixture
+end
+
+for i in 1..2
+  MeetingRoom.create(
+    number: number_fixture,
+    capacity: 25,
+    description: "Sala de reuniões para videoconferência com computador, projetor, 3 câmeras e uma grande mesa redonda com entradas de audio",
+    price: 300.00,
+    occupied: boolean_fixture,
+    videoconf: true
+  )
+  number_fixture += 1
+  boolean_fixture = !boolean_fixture
+end
+
+print "Creating Event Hall"
+for i in 1..2
+  EventHall.create(
+    number: number_fixture,
+    capacity: 100,
+    description: "Salão de eventos com 80 m2. Por padrão, sem serviço de buffet personalizado.",
+    price: 600.00,
+    occupied: boolean_fixture,
+    tables_number: 20
+  )
+  number_fixture += 1
+  boolean_fixture = !boolean_fixture
 end
