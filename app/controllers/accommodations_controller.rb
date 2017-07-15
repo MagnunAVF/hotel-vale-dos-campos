bc4007dd40480b0f4f2033b9d0c864f7650da57b
Magnun Furtado <magnunavf@gmail.com>
2017-07-15 09:57:43 -0700

    refactor: Improve code with reuse

--- app/controllers/accommodations_controller.rb
+++ app/controllers/accommodations_controller.rb
@@ -16,12 +16,8 @@ class AccommodationsController < ApplicationController
     end_date = params[:end_date]
     searched_start_date = Date.new(start_date["start_date(1i)"].to_i, start_date["start_date(2i)"].to_i, start_date["start_date(3i)"].to_i)
     searched_end_date = Date.new(end_date["end_date(1i)"].to_i, end_date["end_date(2i)"].to_i, end_date["end_date(3i)"].to_i)
-    RoomBooking.all.each do |booking|
-      if (booking.start_date..booking.end_date).overlaps?(searched_start_date..searched_end_date)
-        occupied_rooms.append(Room.find(booking.accommodation_id))
-      end
-    end
-    @rooms = Room.all - occupied_rooms
+
+    @rooms = Room.all - search_occupied_rooms(searched_start_date, searched_end_date)
   end
 
   def search_event_halls
@@ -29,12 +25,8 @@ class AccommodationsController < ApplicationController
     searched_period = params[:period]
     start_date = params[:start_date]
     searched_start_date = Date.new(start_date["start_date(1i)"].to_i, start_date["start_date(2i)"].to_i, start_date["start_date(3i)"].to_i)
-    EventHallBooking.all.each do |booking|
-      if searched_start_date.strftime("%Y-%m-%d")==booking.start_date.strftime("%Y-%m-%d") && booking.period.to_i == searched_period.to_i
-        occupied_eventhalls.append(EventHall.find(booking.accommodation_id))
-      end
-    end
-    @event_halls = EventHall.all - occupied_eventhalls
+
+    @event_halls = EventHall.all - search_occupied_event_halls(searched_start_date, searched_period)
   end
 
   def search_meeting_rooms
@@ -43,13 +35,8 @@ class AccommodationsController < ApplicationController
     start_time = params[:start_time]
     searched_start_date = Date.new(start_date["start_date(1i)"].to_i, start_date["start_date(2i)"].to_i, start_date["start_date(3i)"].to_i)
     searched_start_timedate = DateTime.new(start_time["start_time(1i)"].to_i, start_time["start_time(2i)"].to_i, start_time["start_time(3i)"].to_i,start_time["start_time(4i)"].to_i, start_time["start_time(5i)"].to_i, start_time["start_time(6i)"].to_i)
-    MeetingRoomBooking.all.each do |booking|
-      if searched_start_date.strftime("%Y-%m-%d")==booking.start_date.strftime("%Y-%m-%d") &&
-          ((searched_start_timedate.strftime("%H:%M:%S")..(searched_start_timedate + 6.hours).strftime("%H:%M:%S")).overlaps?(booking.start_time.strftime("%H:%M:%S")..(booking.start_time + 6.hours).strftime("%H:%M:%S") ))
-        occupied_rooms.append(MeetingRoom.find(booking.accommodation_id))
-      end
-    end
-    @meeting_rooms = MeetingRoom.all - occupied_rooms
+
+    @meeting_rooms = MeetingRoom.all - search_occupied_meeting_rooms(searched_start_date, searched_start_timedate)
   end
 
   def show
